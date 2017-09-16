74d6874b15fe863da74b370127e9f4b3eadb9ba0
Live session user <vinodarajugm@gmail.com>
2017-09-16 13:07:29 +0000

    Added login

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,15 +1,30 @@
 package controllers;
 
 import actors.MessageActor;
-import play.mvc.*;
-
+import data.LoginForm;
+import play.Configuration;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.LegacyWebSocket;
+import play.mvc.Result;
+import play.mvc.Results;
+import play.mvc.WebSocket;
 import views.html.*;
+
+import javax.inject.Inject;
+import java.util.Objects;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+   @Inject
+   Configuration configuration;
+
+   @Inject
+   FormFactory formFactory;
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -24,4 +39,16 @@ public class HomeController extends Controller {
     public LegacyWebSocket<String> chatSocket() {
         return WebSocket.withActor(MessageActor::props);
     }
+    public Result login() {
+       return ok(views.html.login.render(""));
+    }
+
+   public Result authenticate(){
+       Form<LoginForm> loginFormForm = formFactory.form(LoginForm.class).bindFromRequest();
+       if(Objects.equals(configuration.getString("app.user.username"), loginFormForm.get().getUsername()) &&
+               Objects.equals(configuration.getString("app.user.password"), loginFormForm.get().getPassword())){
+           return ok();
+       }
+       return Results.unauthorized();
+   }
 }
